4aaeb5d14c75aaa7df00a8201c3258532800d0cb
Kapil Singhal <152382456+KapilSinghal21@users.noreply.github.com>
2025-11-17 09:08:18 +0530

    Time: 0 ms (100%), Space: 61.4 MB (74.91%) - LeetHub

---
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int last = -1;  // stores index of previous 1
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                if (last != -1 && i - last - 1 < k) {
+                    return false;
+                }
+                last = i;  // update last seen position
+            }
+        }
+        return true;
+    }
+};
